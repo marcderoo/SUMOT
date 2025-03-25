409d1253335f30c1bffb90f3e53bc19841f42dfe
Arnaud COURNIL <arnaud.cournil@edu.devinci.fr>
2025-03-25 18:56:18 +0100

    Correct tests unitaires

--- tests/test_tusmo_app.py
+++ tests/test_tusmo_app.py
@@ -25,40 +25,6 @@ def get_path(full_path):
         full_path = full_path.replace("\\", "/")
     return str(os.path.join(root, full_path))
 
-def run_tests():
-    """
-    Run all specified test files and return whether they pass.
-    """
-    test_files = [
-        get_path("app/experiments/mode_duel_test.py"),
-        get_path("app/experiments/mode_battleIA_test.py"),
-        get_path("app/experiments/solveur_test.py"),
-    ]
-
-    env = os.environ.copy()
-    env["PYTHONIOENCODING"] = "utf-8"  # Force utf-8 encoding
-
-    res = True
-    for test_file in test_files:
-        print(f"Running tests in {test_file}...")
-        result = subprocess.run(
-            [sys.executable, "-m", "unittest", test_file],
-            capture_output=True,
-            text=True,
-            env=env,  # Use the updated environment
-        )
-
-        if result.returncode != 0:
-            print(f"❌ Tests failed in {test_file}:")
-            print(result.stdout)
-            print(result.stderr)
-            res = False
-            continue
-
-        print(f"✅ Tests passed in {test_file}.")
-
-    return res
-
 
 def test_flask_app():
     """
@@ -89,7 +55,7 @@ def test_flask_app():
 
 
 if __name__ == "__main__":
-    if run_tests() and test_flask_app():
+    if test_flask_app():
         print("✅ All tests passed")
     else:
         print("❌ Tests failed.")
